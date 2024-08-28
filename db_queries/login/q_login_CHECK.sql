59c5162006d5d5e9c4642b34a0d6b17a700b5652
nightramm <rnemartin.calpo@gmail.com>
2024-08-28 22:09:44 +0800

    Update q_login_CHECK.sql

--- db_queries/login/q_login_CHECK.sql
+++ db_queries/login/q_login_CHECK.sql
@@ -1,6 +1,6 @@
-SELECT CASE 
-         WHEN (user_ID = '$user_ID' AND user_password = '$user_password') THEN 'Login successful'
-         ELSE 'Login failed'
-       END AS login_status
+SELECT 
+  IF(user_ID = '$user_ID' AND user_password = '$user_password', 'Login successful', 'Login failed') AS login_status,
+  IF(user_ID = '$user_ID' AND user_password = '$user_password', user_ID, NULL) AS entered_user_ID,
+  IF(user_ID = '$user_ID' AND user_password = '$user_password', user_type, NULL) AS user_role
 FROM user_login
 WHERE user_ID = '$user_ID';
