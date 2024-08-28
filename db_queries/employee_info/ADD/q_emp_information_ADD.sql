f48282f7b4a5b4a83733a0a2477c73dd3f15b185
nightramm <rnemartin.calpo@gmail.com>
2024-08-28 22:59:47 +0800

    Update and rename q_employee_information_ADD.sql to q_emp_information_ADD.sql

--- db_queries/employee_info/ADD/q_emp_information_ADD.sql
+++ db_queries/employee_info/ADD/q_emp_information_ADD.sql
@@ -1,6 +1,6 @@
 INSERT INTO employee_information (user_ID, first_name, middle_name, last_name)
 VALUES (
-  '$user_ID',  
-  '$fname', 
-  '$mname', 
-  '$lname');
+        '$user_ID',  
+        '$fname', 
+        '$mname', 
+        '$lname');
