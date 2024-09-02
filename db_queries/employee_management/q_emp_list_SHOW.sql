f30c7bed4b9c070f381faf14ace7aaf089fe800a
nightramm <rnemartin.calpo@gmail.com>
2024-09-02 22:37:03 +0800

    Create q_emp_list_SHOW.sql

---
+++ db_queries/employee_management/q_emp_list_SHOW.sql
@@ -0,0 +1,12 @@
+SELECT i.user_ID AS ID, 
+	CONCAT(i.first_name, ' ', i.last_name) AS NAME,
+    d.department_name AS DEPARTMENT,
+    c.contact_no AS CONTACT,
+    COUNT(CASE WHEN l.manager_approval = 0 OR l.hr_approval = 0 THEN 1 END) OVER (PARTITION BY l.employee_ID) AS REQUESTS,
+    start_date AS START_DATE
+ FROM employee_history h
+ LEFT JOIN employee_information i ON h.employee_ID = i.employee_ID
+ LEFT JOIN employee_department d ON h.department_ID = d.department_ID
+ LEFT JOIN employee_contactinfo c ON h.employee_ID = c.employee_ID
+ LEFT JOIN employee_leave l ON h.employee_ID = l.employee_ID
+ WHERE NOT status = 0;
