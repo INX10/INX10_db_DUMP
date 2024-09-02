93c9e1d1eb468e44d15ce439b172e46a460cccb6
nightramm <rnemartin.calpo@gmail.com>
2024-09-02 21:36:09 +0800

    Update q_emp_info_SHOW (-2).sql

--- db_queries/employee_info/q_emp_info_SHOW (-2).sql
+++ db_queries/employee_info/q_emp_info_SHOW (-2).sql
@@ -1,9 +1,9 @@
-SELECT i.user_ID AS i_user_ID, i.first_name AS i_first_name, i.middle_name AS i_middle_name, i.last_name AS i_last_name, 
-	j.job_title AS job, d.department_name AS department, st.start_date,
+SELECT i.user_ID AS i_employee_ID, CONCAT(i.first_name, ' ', i.middle_name, ' ', i.last_name) AS employee_name, 
+	CONCAT(j.job_title, ', ', d.department_name) AS emp_job, st.start_date,
 	c.email AS c_email, c.contact_no AS c_contact_no, c.telephone_no AS c_telephone_no, c.permanent_address AS c_permanent_address, c.current_address AS c_current_address,
-        o.birth_date AS o_birth_date, o.birth_place AS o_birth_place, o.civil_status AS o_civil_status,
-        ed.highschool AS ed_highschool, ed.college AS ed_college, ed.remarks AS ed_remarks,
-        g.sss AS g_sss, g.philhealth AS g_philhealth, g.pagibig AS g_pagibig, g.tin AS g_tin
+	o.birth_date AS o_birth_date, o.birth_place AS o_birth_place, o.civil_status AS o_civil_status,
+    	ed.highschool AS ed_highschool, ed.college AS ed_college, ed.remarks AS ed_remarks,
+    	g.sss AS g_sss, g.philhealth AS g_philhealth, g.pagibig AS g_pagibig, g.tin AS g_tin
 FROM employee_information i
 LEFT JOIN employee_contactinfo c ON i.employee_ID = c.employee_ID
 LEFT JOIN employee_otherinfo o ON i.employee_ID = o.employee_ID
@@ -12,6 +12,6 @@ LEFT JOIN employee_governmentid g ON i.employee_ID = g.employee_ID
 LEFT JOIN employee_history st ON i.employee_ID = st.employee_ID 
 LEFT JOIN employee_department d ON st.department_ID = d.department_ID
 LEFT JOIN employee_job j ON st.job_ID = j.job_ID
-WHERE i.employee_ID = 1;
+WHERE i.employee_ID = 4;
 
 --WITHOUT WORK_HISTORY AND DOCUMENTS [REASON: POSSIBLE MULTIPLE ENTRIES]
