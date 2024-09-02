e8cbfe145d8522cef98b33bef767b628c6c02c41
nightramm <rnemartin.calpo@gmail.com>
2024-09-02 16:54:32 +0800

    Create q_announce_ADD.sql

---
+++ db_queries/announcement/q_announce_ADD.sql
@@ -0,0 +1,6 @@
+INSERT INTO announcement (employee_ID, announce_subject, announce_body, date)
+VALUES 
+	($emp_ID, 
+    '$announce_subject', 
+    '$announce_body', 
+    '$date_time [YYYY-MM-DD HH:MM:SS]');
